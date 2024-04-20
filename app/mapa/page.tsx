d6f671a96c863b250b1243951e145bcb935b66ba
3rickDJ <erick.dejesus1@alumno.buap.mx>
2024-04-20 01:28:09 -0600

    integrate changes of jonatan

--- app/mapa/page.tsx
+++ app/mapa/page.tsx
@@ -1,30 +1,75 @@
 'use client'
 import React from 'react';
 import Mapa from './mapa';
-import { Box, Container, Grid } from '@mui/material';
+import { Box, Typography, Button} from '@mui/material';
 import shadows from '@mui/material/styles/shadows';
 
 
 export default function Page() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent:"center",
-        height: "100vh",
-      }}
-    >
-      <Box sx={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'white',
-        position: 'relative',
-        color: 'black',
-      }}>
-        <Mapa />
+    <>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          bgcolor: '#FF5A00',
+          color: 'white',
+          padding: 4, // Aumenta el espacio dentro de la caja
+          textAlign: 'center', // Centra el texto dentro de la caja
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Selecciona tu espacio de aparcamiento
+        </Typography>
       </Box>
-    </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '200px', // Distancia desde arriba
+          left: '50%', // Centra horizontalmente
+          transform: 'translateX(-50%)', // Centra horizontalmente
+          width: '80%', // Ancho del 80% del contenedor padre
+          height: '500px', // Altura de 500px
+          margin: '10px auto', // Margen superior e inferior de 20px, y auto para centrar horizontalmente
+          bgcolor: '#ECE9E6',
+          padding: 4,
+          textAlign: 'center',
+          borderRadius: 2, // Bordes redondeados
+        }}
+      >
+        <Typography variant="h4" gutterBottom color={'black'}>
+          Mapa de aparcamiento
+        </Typography>
+        <Box
+          sx={{
+            position: 'relative',
+            height: '100%',
+            width: '100%',
+          }}>
+          <Mapa />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '750px', // Ajusta la posiciÃ³n vertical de los botones
+          left: '50%', // Centra horizontalmente
+          transform: 'translateX(-50%)', // Centra horizontalmente
+          width: '80%', // Ancho del 80% del contenedor padre
+          textAlign: 'center',
+          marginBottom: '40px', // Aumenta el margen inferior
+          marginTop: '20px', // Aumenta el margen superior
+        }}
+      >
+        <Button sx={{ mr: 2, width: '200px', height: '50px' }} variant="outlined" color="error">
+          Regresar
+        </Button>
+        <Button sx={{ width: '200px', height: '50px' }} variant="contained" color="success">
+          Continuar
+        </Button>
+      </Box>
+    </>
   );
 }
