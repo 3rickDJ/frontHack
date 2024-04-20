7813193123e096f1ea91087930bff74b2dc4c072
3rickDJ <erick.dejesus1@alumno.buap.mx>
2024-04-19 20:21:39 -0600

    Code API fro deploy

--- app/api/apiBack.js
+++ app/api/apiBack.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-// const API = process.env.NEXT_PUBLIC_API_CRM
-console.log(process.env.NEXT_PUBLIC_BACK)
-const API = process.env.NEXT_PUBLIC_BACK
+const API = "https://my-strapi-project2-i37b2ut65q-uc.a.run.app/"
 
-console.log('hola')
-console.log(process.env.NEXT_PUBLIC_BACK)
-console.log('hola')
 export default axios.create({
   baseURL: `${API}/api`,
 })
