bfb6cffedcc0ce2362cd26b2b9baafa64f07adac
3rickDJ <erick.dejesus1@alumno.buap.mx>
2024-04-19 20:31:11 -0600

    Refactor main menu

--- app/component/main-menu.tsx
+++ app/component/main-menu.tsx
@@ -4,46 +4,19 @@ import { Box, styled, Chip} from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { Colors } from '../theme/colors'
 import { getLocations } from '../services/back/locations'
-import axios from 'axios'
 import Mapa from './Mapa'
-
-const api = axios.create({
-  baseURL: 'https://my-strapi-project2-i37b2ut65q-uc.a.run.app/api/'
-})
-
-
-const fetchUser = async () => {
-  const response = await api({
-    method: 'GET',
-    url: '/locations'
-  })
-  return response.data
-}
-
-const MainMenuFrame = styled(Box, {
-    name: 'MainMenuFrameComponent',
-    slot: "Root"
-})(({ }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '6px 8px',
-    background: `radial-gradient(circle, rgba(0,0,0,0.2) 10%,
-    rgba(0,0,0,1)), url("images/main-menu-bg.webp")`,
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    gap: 4,
-    boxShadow: `2px 2px 16px 2px ${Colors.secondary}`
-}))
 
 function MainMenu() {
 
   const [locations, setLocations] =  useState([])
   useEffect(() => {
     const fetchData = async () => {
-      const response = await getLocations()
-      setLocations(response.data.data)
+      try {
+        const response = await getLocations()
+        setLocations(response.data.data)
+      } catch (error) {
+        console.log('Error fetching locations', error)
+      }
     }
     fetchData()
   }, [])
