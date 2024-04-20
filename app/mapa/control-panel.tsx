15419e9ff6013e7157f3977a5e0e6bebfb032589
Erick De Jesus <94492584+3rickDJ@users.noreply.github.com>
2024-04-20 01:36:33 -0600

    3 research wrapper google mapjs (#8)
    * add page mapa
    
    * add demo of markers
    
    * add box
    
    * full page size
    
    * integrate changes of jonatan

--- app/mapa/control-panel.tsx
+++ app/mapa/control-panel.tsx
@@ -1,26 +1,36 @@
+import {Box} from '@mui/material';
 import * as React from 'react';
 
 function ControlPanel() {
   return (
-    <div className="control-panel">
-      <h3>Markers and InfoWindows</h3>
-      <p>
-        This example shows the different ways to add markers and infowindows to
-        the map.
-      </p>
-      <div className="links">
-        <a
-          href="https://codesandbox.io/s/github/visgl/react-google-maps/tree/main/examples/markers-and-infowindows"
-          target="_new">
-          Try on CodeSandbox ↗
-        </a>
+    <div>
 
-        <a
-          href="https://github.com/visgl/react-google-maps/tree/main/examples/markers-and-infowindows"
-          target="_new">
-          View Code ↗
-        </a>
-      </div>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+        }}
+      >
+        <h3>Markers and InfoWindows</h3>
+        <p>
+          This example shows the different ways to add markers and infowindows to
+          the map.
+        </p>
+        <div className="links">
+          <a
+            href="https://codesandbox.io/s/github/visgl/react-google-maps/tree/main/examples/markers-and-infowindows"
+            target="_new">
+            Try on CodeSandbox ↗
+          </a>
+
+          <a
+            href="https://github.com/visgl/react-google-maps/tree/main/examples/markers-and-infowindows"
+            target="_new">
+            View Code ↗
+          </a>
+        </div>
+      </Box>
     </div>
   );
 }
